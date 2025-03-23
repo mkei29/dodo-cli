f867d45b3c005b88117ae45c4f9a97bf72e01f63
Keisuke Miura <toritorineko29@gmail.com>
2025-03-23 21:49:43 +0900

    feat: adjust apperance

--- src/cmd_search.go
+++ src/cmd_search.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"net/http"
+	"net/url"
 	"os"
 	"os/exec"
 	"runtime"
@@ -15,8 +17,21 @@ import (
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/x/ansi"
 	"github.com/charmbracelet/x/term"
 	"github.com/spf13/cobra"
+)
+
+const (
+	PrimaryColorLight = "#F3A200"
+	PrimaryColorDark  = "#FFCC66"
+	TextColorLight0   = "#000000"
+	TextColorLight1   = "#333333"
+	TextColorDark0    = "#FFFFFF"
+	TextColorDark1    = "#DADADA"
+	TextColorLightDim = "#666666"
+	TextColorDarkDim  = "#888888"
+	MaxWidth          = 100
 )
 
 type SearchArgs struct {
@@ -40,17 +55,18 @@ func CreateSearchCmd() *cobra.Command {
 	return searchCmd
 }
 
-type item struct {
+type searchItem struct {
 	title       string
 	description string
 	url         string
+	domain      string
 }
 
-func (i item) FilterValue() string { return i.title }
+func (i searchItem) FilterValue() string { return i.title }
 
-func (i item) Title() string { return i.title }
+func (i searchItem) Title() string { return i.title }
 
-func (i item) Description() string { return i.description }
+func (i searchItem) Description() string { return i.description }
 
 type model struct {
 	textInput       textinput.Model
@@ -71,6 +87,7 @@ func initialModel(args SearchArgs, env EnvArgs) model {
 
 	w, h, _ := term.GetSize(os.Stdout.Fd())
 	ti := textinput.New()
+	ti.Prompt = ""
 	ti.Placeholder = "Search..."
 	ti.Focus()
 	ti.CharLimit = 255
@@ -78,7 +95,7 @@ func initialModel(args SearchArgs, env EnvArgs) model {
 
 	// Set appropriate dimensions for the list
 	items := []list.Item{}
-	l := list.New(items, list.NewDefaultDelegate(), w-2, h-4) // Width: 30, Height: 10
+	l := list.New(items, newSearchItemDelegate(), w-2, h-4) // Width: 30, Height: 10
 	l.Title = ""
 	l.Styles = listStyles
 	l.SetShowTitle(false)
@@ -139,7 +156,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) { //nolint: ireturn
 
 func (m model) updateEnter(msg tea.Msg) (tea.Model, tea.Cmd) { //nolint: ireturn
 	if !m.textInputActive {
-		selectedItem, ok := m.list.SelectedItem().(item)
+		selectedItem, ok := m.list.SelectedItem().(searchItem)
 		if !ok {
 			m.errorMessage = "No item selected"
 			return m, nil
@@ -153,6 +170,7 @@ func (m model) updateEnter(msg tea.Msg) (tea.Model, tea.Cmd) { //nolint: ireturn
 
 	m.textInput.Blur()
 	query := m.textInput.Value()
+
 	records, err := sendSearchRequest(m.envArgs, m.args.endpoint, query)
 	if err != nil {
 		m.errorMessage = fmt.Sprintf("failed to execute search: %s", err)
@@ -161,10 +179,20 @@ func (m model) updateEnter(msg tea.Msg) (tea.Model, tea.Cmd) { //nolint: ireturn
 	// Update the list
 	items := make([]list.Item, len(records))
 	for i, record := range records {
-		items[i] = item{
+		var domain string
+		parsedURL, err := url.Parse(record.Url)
+
+		if err == nil {
+			domain = parsedURL.Hostname()
+		} else {
+			domain = "unknown"
+		}
+
+		items[i] = searchItem{
 			title:       record.Title,
 			description: record.Contents,
 			url:         record.Url,
+			domain:      domain,
 		}
 	}
 	m.list.SetItems(items)
@@ -177,7 +205,10 @@ func (m model) updateEnter(msg tea.Msg) (tea.Model, tea.Cmd) { //nolint: ireturn
 }
 
 func (m model) View() string {
-	text := fmt.Sprintf("Search %s\n\n", m.textInput.View())
+	searchStyle := lipgloss.NewStyle().
+		Foreground(lipgloss.AdaptiveColor{Light: PrimaryColorLight, Dark: PrimaryColorDark}).
+		Bold(true)
+	text := fmt.Sprintf("%s %s\n\n", searchStyle.Render("Search >"), m.textInput.View())
 	text += m.list.View() + "\n"
 
 	if m.errorMessage != "" {
@@ -188,10 +219,135 @@ func (m model) View() string {
 	return text
 }
 
+// Implementations for the list item UI
+type searchItemDelegate struct {
+	styles searchItemDelegateStyles
+}
+
+func newSearchItemDelegate() searchItemDelegate {
+	return searchItemDelegate{
+		styles: newSearchItemStyles(),
+	}
+}
+
+func (d searchItemDelegate) Render(w io.Writer, m list.Model, index int, item list.Item) {
+	// This function assume that the item has searchItem type.
+	sitem, ok := item.(searchItem)
+	if !ok {
+		return
+	}
+
+	// params
+	var (
+		title       string
+		description string
+		hostname    string
+	)
+
+	width := min(m.Width(), MaxWidth) - 1
+	lines := strings.Split(ansi.Hardwrap(sitem.description, width, false), "\n")
+	if len(lines) > 2 {
+		lines = lines[:2]
+		lastLine := lines[1]
+		lines[1] = fmt.Sprintf("%s...", lastLine[:len(lastLine)-5])
+	}
+	adjustedDescription := strings.Join(lines, "\n")
+
+	isSelected := index == m.Index()
+	if isSelected {
+		title = d.styles.SelectedTitle.Render(sitem.title)
+		description = d.styles.SelectedDescription.Render(adjustedDescription)
+		hostname = d.styles.SelectedHostname.Render(sitem.domain)
+	} else {
+		title = d.styles.Title.Render(sitem.title)
+		description = d.styles.Description.Render(adjustedDescription)
+		hostname = d.styles.Hostname.Render(sitem.domain)
+	}
+	fmt.Fprintf(w, "%s %s\n", title, hostname)
+	fmt.Fprintf(w, "%s\n", description)
+}
+
+func (d searchItemDelegate) Height() int {
+	// Return the height of each list item including spacing
+	return 4
+}
+
+func (d searchItemDelegate) Spacing() int {
+	// Return the spacing between list items
+	return 1
+}
+
+func (d searchItemDelegate) Update(msg tea.Msg, m *list.Model) tea.Cmd {
+	// Handle item-level updates if necessary
+	return nil
+}
+
 func initListStyles() list.Styles {
 	styles := list.DefaultStyles()
 	styles.StatusBar = lipgloss.NewStyle().Foreground(lipgloss.AdaptiveColor{Light: "#A49FA5", Dark: "#777777"})
 	return styles
+}
+
+type searchItemDelegateStyles struct {
+	Title               lipgloss.Style
+	SelectedTitle       lipgloss.Style
+	Hostname            lipgloss.Style
+	SelectedHostname    lipgloss.Style
+	Description         lipgloss.Style
+	SelectedDescription lipgloss.Style
+}
+
+func newSearchItemStyles() searchItemDelegateStyles {
+	return searchItemDelegateStyles{
+		Title: lipgloss.NewStyle().
+			Foreground(lipgloss.AdaptiveColor{
+				Light: TextColorLight0,
+				Dark:  TextColorDark0,
+			}).
+			Bold(true).
+			Padding(0, 0, 0, 2),
+		SelectedTitle: lipgloss.NewStyle().
+			Foreground(lipgloss.AdaptiveColor{
+				Light: PrimaryColorLight,
+				Dark:  PrimaryColorDark,
+			}).
+			Bold(true).
+			Border(lipgloss.NormalBorder(), false, false, false, true).
+			BorderForeground(lipgloss.AdaptiveColor{
+				Light: PrimaryColorLight,
+				Dark:  PrimaryColorDark,
+			}).
+			Padding(0, 0, 0, 1),
+		Hostname: lipgloss.NewStyle().
+			Foreground(lipgloss.AdaptiveColor{
+				Light: TextColorLightDim,
+				Dark:  TextColorDarkDim,
+			}).
+			Padding(0, 0, 0, 1),
+		SelectedHostname: lipgloss.NewStyle().
+			Foreground(lipgloss.AdaptiveColor{
+				Light: TextColorLightDim,
+				Dark:  TextColorDarkDim,
+			}).
+			Padding(0, 0, 0, 1),
+		Description: lipgloss.NewStyle().
+			Foreground(lipgloss.AdaptiveColor{
+				Light: TextColorLight1,
+				Dark:  TextColorDark1,
+			}).
+			Padding(0, 0, 0, 2),
+		SelectedDescription: lipgloss.NewStyle().
+			Foreground(lipgloss.AdaptiveColor{
+				Light: TextColorLight0,
+				Dark:  TextColorDark0,
+			}).
+			Border(lipgloss.NormalBorder(), false, false, false, true).
+			BorderForeground(lipgloss.AdaptiveColor{
+				Light: PrimaryColorLight,
+				Dark:  PrimaryColorDark,
+			}).
+			Padding(0, 0, 0, 1),
+	}
 }
 
 func executeSearch(_ *cobra.Command, args SearchArgs) error {
